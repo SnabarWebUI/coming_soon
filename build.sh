397d7df8e6123569df114e34c9822544bf4286e1
Akash Shinde <vertisinfotechakashs@ooyala.com>
2016-06-15 20:00:52 +0530

    ci script added for docker deployment

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+docker build -t snabar/coming-soon .
+echo "$(tput setaf 2)Building image $(tput sgr0)"
+docker run --rm --name snabar-coming-soon snabar/coming-soon
+echo "$(tput setaf 2)Running container for the first time $(tput sgr0)"
+
+if [ $? != 0 ]
+then
+    docker stop snabar-coming-soon
+    echo "$(tput setaf 1)Stopping Docker image $(tput sgr0)"
+    docker rm snabar-coming-soon
+    echo "$(tput setaf 1)Removing Docker image $(tput sgr0)"
+    docker run --rm --name snabar-coming-soon -p 80:80 snabar/coming-soon
+    echo "$(tput setaf 1)Starting new Docker image $(tput sgr0)"
+fi
+
