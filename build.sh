0a24857c36fe29da8d8d8be3a8644eb300da73d8
Akash Shinde <vertisinfotechakashs@ooyala.com>
2016-06-15 20:09:43 +0530

    detatching container

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 docker build -t snabar/coming-soon .
 echo "$(tput setaf 2)Building image $(tput sgr0)"
-docker run --rm --name snabar-coming-soon -p 80:80 snabar/coming-soon
+docker run --rm --name snabar-coming-soon -p 80:80 -d snabar/coming-soon
 echo "$(tput setaf 2)Running container for the first time $(tput sgr0)"
 
 if [ $? != 0 ]
@@ -11,7 +11,6 @@ then
     echo "$(tput setaf 1)Stopping Docker image $(tput sgr0)"
     docker rm snabar-coming-soon
     echo "$(tput setaf 1)Removing Docker image $(tput sgr0)"
-    docker run --rm --name snabar-coming-soon -p 80:80 snabar/coming-soon
+    docker run --rm --name snabar-coming-soon -p 80:80 -d snabar/coming-soon
     echo "$(tput setaf 1)Starting new Docker image $(tput sgr0)"
 fi
-
