b9e440563e0a8ea19986d76b7d5298892b98e518
Akash Shinde <vertisinfotechakashs@ooyala.com>
2016-06-15 20:11:00 +0530

    fixing docker conflict issue

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 docker build -t snabar/coming-soon .
 echo "$(tput setaf 2)Building image $(tput sgr0)"
-docker run --rm --name snabar-coming-soon -p 80:80 -d snabar/coming-soon
+docker run -d --name snabar-coming-soon -p 80:80 snabar/coming-soon
 echo "$(tput setaf 2)Running container for the first time $(tput sgr0)"
 
 if [ $? != 0 ]
