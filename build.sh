749d4db661e1a0815daf5951aebe2f279eb0cb69
Akash Shinde <vertisinfotechakashs@ooyala.com>
2016-06-15 21:06:25 +0530

    fixing issue with ci script

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 
 docker build -t snabar/coming-soon .
 echo "$(tput setaf 2)Building image $(tput sgr0)"
+docker stop snabar-coming-soon
+echo "$(tput setaf 1)Stopping Docker image $(tput sgr0)"
+docker rm snabar-coming-soon
+echo "$(tput setaf 1)Removing Docker image $(tput sgr0)"
 docker run -d --name snabar-coming-soon -p 80:80 snabar/coming-soon
 
 if [ $? != 0 ]
